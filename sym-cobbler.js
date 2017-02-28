816110ce0c80b241f26b8cbb6e5528dc2fa35494
Virtual Enviroment <bsergent@osisoft.com>
2017-02-28 21:36:46 +0000

    Add horizontal resizing
    Not vertical yet because it's hard to make the canvas take up all the
    extra vertical space.

--- sym-cobbler.js
+++ sym-cobbler.js
@@ -11,25 +11,30 @@
     function symbolVis() { }
     CS.deriveVisualizationFromBase(symbolVis);
 
+    var canvas;
     var context;
-    var tileWidth = 30;
+    var scaleX;
+    var scaleY;
+    var dataWidth = 5;
+    var dataHeight = 5;
 
     // Intializer
     symbolVis.prototype.init = function (scope) {
 
-        context = document.getElementById('cobblerCanvas').getContext('2d');//$('#cobblerCanvas')[0].getContext('3d');
+        canvas = $('#cobblerCanvas');
+        context = canvas[0].getContext('2d');
 
         // Do this every data update (every 5s)
-        this.onDataUpdate = dataUpdate;
-        function dataUpdate(data) {
+        this.onDataUpdate = function (data) {
+            updateScale(); // TODO This should be in a resize listener in case data is frozen
             var color = [];
-            for (var y = 0; y < 5; y++) {
-                for (var x = 0; x < 5; x++) {
+            for (var y = 0; y < dataHeight; y++) {
+                for (var x = 0; x < dataWidth; x++) {
                     // Boxes
                     for (var c = 0; c < 3; c++) color[c] = Math.round(Math.random() * 9);
                     context.beginPath();
                     context.fillStyle = '#' + color[0] + '' + color[1] + '' + color[2];
-                    context.rect(x * tileWidth, y * tileWidth, tileWidth, tileWidth);
+                    context.rect(x * scaleX, y * scaleY, scaleX, scaleY);
                     context.fill();
                     context.closePath();
 
@@ -38,7 +43,7 @@
                     context.beginPath();
                     context.fillStyle = '#' + color[0] + '' + color[1] + '' + color[2];
                     context.font = '12px Verdana';
-                    context.fillText(color[0] + color[1] + color[2], (x * tileWidth) + (tileWidth / 3), (y * tileWidth) + (tileWidth * 2.5 / 4));
+                    context.fillText(color[0] + color[1] + color[2], (x * scaleX) + (scaleX / 3), (y * scaleY) + (scaleY * 2.5 / 4));
                     context.closePath();
                 }
             }
@@ -51,6 +56,16 @@
                     scope.label = data.Label;
                 }
             }
+        }
+        
+        function updateScale() {
+            canvas.attr('width', canvas.width());
+            canvas.attr('height', canvas.height());
+            scaleX = canvas.width() / dataWidth;
+            scaleY = canvas.height() / dataHeight;
+            scaleX = Math.floor(scaleX);
+            scaleY = Math.floor(scaleY);
+            console.log('canvasWidth (' + canvas.width() + ') / dataWidth (' + dataWidth + ') = scaleX (' + scaleX + ')');
         }
     };
 
