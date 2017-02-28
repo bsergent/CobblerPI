5c2de7922542375908968d1306f84180db82fa5f
Virtual Enviroment <bsergent@osisoft.com>
2017-02-28 20:18:20 +0000

    Render 5x5 grid of random colors and numbers
    This is just getting something actually drawn on the canvas. This could
    be the start of a heatmap as well since the 3d part is pretty hard. This
    does not display any real values. One big thing we need to look at is
    dynamically resizing the canvas.

--- sym-cobbler.js
+++ sym-cobbler.js
@@ -13,16 +13,37 @@
     function symbolVis() { }
     CS.deriveVisualizationFromBase(symbolVis);
 
-    var context = $('#context')[0].getContext('3d');
+    var context;
+    var tileWidth = 30;
 
-    // Intiializer
+    // Intializer
     symbolVis.prototype.init = function (scope) {
 
-        // Do this every data update
+        context = document.getElementById('cobblerCanvas').getContext('2d');//$('#cobblerCanvas')[0].getContext('3d');
+
+        // Do this every data update (every 5s)
         this.onDataUpdate = dataUpdate;
         function dataUpdate(data) {
-            //context.rect();
-            //context.fill();
+            var color = [];
+            for (var y = 0; y < 5; y++) {
+                for (var x = 0; x < 5; x++) {
+                    // Boxes
+                    for (var c = 0; c < 3; c++) color[c] = Math.round(Math.random() * 9);
+                    context.beginPath();
+                    context.fillStyle = '#' + color[0] + '' + color[1] + '' + color[2];
+                    context.rect(x * tileWidth, y * tileWidth, tileWidth, tileWidth);
+                    context.fill();
+                    context.closePath();
+
+                    // Labels
+                    for (var c = 0; c < 3; c++) color[c] = 9 - color[c];
+                    context.beginPath();
+                    context.fillStyle = '#' + color[0] + '' + color[1] + '' + color[2];
+                    context.font = '12px Verdana';
+                    context.fillText(color[0] + color[1] + color[2], (x * tileWidth) + (tileWidth / 3), (y * tileWidth) + (tileWidth * 2.5 / 4));
+                    context.closePath();
+                }
+            }
 
             if (data) {
                 // Add variables to angular scope
